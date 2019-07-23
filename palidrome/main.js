e629b625abd52fc4bf0a6cca4361d3a9a1c0fef1
Angelika Kachur <angelika.kachur@gmail.com>
2019-07-23 16:13:13 +0300

    Punctuation and UpperCase checking in Polidrome

--- palidrome/main.js
+++ palidrome/main.js
@@ -1,9 +1,11 @@
 function isPalidrome(str) {
-    let strArr = str.split(""); //array from original string
+    let cleanStr = str.replace(/[^\w\s]|_/g, "").toLowerCase(); //regExp for deleting everything except alph characters and whitespace
+    let strArr = cleanStr.split(""); //array from original string
     let reverseStrArr = strArr.reverse(); //reverse array
     let reverseStr = reverseStrArr.join(''); //male string from reverse arrey without ','
+    console.log(reverseStr);
 
-    if (str == reverseStr) {
+    if (cleanStr == reverseStr) {
         return true;
     } else {
         return false;
@@ -12,3 +14,5 @@ function isPalidrome(str) {
 
 console.log(isPalidrome("notapalidrome")); //false
 console.log(isPalidrome("niin")); //true
+console.log(isPalidrome("Niin")); //true even though has capital letter
+console.log(isPalidrome("n.iin")); //true even though has dot
