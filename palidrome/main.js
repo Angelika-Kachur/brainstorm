9be8767e3f7afeed77ee97b8b7b27430ef09b2da
Angelika Kachur <angelika.kachur@gmail.com>
2019-07-24 23:58:52 +0300

    started shorter isPalidrome function

--- palidrome/main.js
+++ palidrome/main.js
@@ -1,18 +1,27 @@
 function isPalidrome(str) {
-    let cleanStr = str.replace(/[^\w\s]|_/g, "").toLowerCase(); //regExp for deleting everything except alph characters and whitespace
-    let strArr = cleanStr.split(""); //array from original string
+    let cleanStr = str.replace(/[^\w\s]|_/g, '').toLowerCase(); //regExp for deleting everything except alph characters and whitespace
+    let strArr = cleanStr.split(''); //array from original string
     let reverseStrArr = strArr.reverse(); //reverse array
     let reverseStr = reverseStrArr.join(''); //male string from reverse arrey without ','
     console.log(reverseStr);
 
-    if (cleanStr == reverseStr) {
+    /* if (cleanStr == reverseStr) {
         return true;
     } else {
         return false;
-    }
+    } */
+    //refactoring
+
+    return cleanStr == reverseStr;
 }
 
-console.log(isPalidrome("notapalidrome")); //false
-console.log(isPalidrome("niin")); //true
-console.log(isPalidrome("Niin")); //true even though has capital letter
-console.log(isPalidrome("n.iin")); //true even though has dot
+console.log(isPalidrome('notapalidrome')); //false
+console.log(isPalidrome('niin')); //true
+console.log(isPalidrome('Niin')); //true even though has capital letter
+console.log(isPalidrome('n.iin')); //true even though has dot
+
+function isPalidromeShorter(str) {
+    console.log(str);
+}
+
+console.log(isPalidromeShorter('niin')); //true
